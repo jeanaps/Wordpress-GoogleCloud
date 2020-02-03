e926c5aa72113fbae4a3e2cb9eb5b904419d8a53
jeanaps <jean.aps12@gmail.com>
2020-02-03 13:14:54 -0300

    Ajuste path id_rsa

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "google_compute_instance" "helloworld" {
 #----------------------------------------------
 # Cria SQL Gerenciado com Engine MySQL 
 resource "google_sql_database_instance" "wordpressdb" {
-  name             = "database-1"
+  name             = "database-wp"
   database_version = var.db-version
   region           = var.region
   settings {
